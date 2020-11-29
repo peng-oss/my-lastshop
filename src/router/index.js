3b47786e371c8469614ac1e8c7296871eabced2a
peng-oss <pengjiahuizhibang@qq.com>
2020-11-29 23:37:20 +0800

    完成数据报表

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/params.vue'
 import List from '../components/goods/list.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/report.vue'
 Vue.use(VueRouter)
 
 
@@ -52,6 +53,8 @@ const router = new VueRouter({
       },
       {
         path:'/orders',component:Order
+      },{
+        path:'/reports',component:Report
       },
     ]
     },
